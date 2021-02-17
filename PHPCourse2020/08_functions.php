140fe6f4337c479c26bf1f674d8fed09faf6320f
Sebastian Reichenbach <reichenbach.s@gmail.com>
2021-02-17 13:33:05 +0100

    seb, chapter 09 done

--- PHPCourse2020/08_functions.php
+++ PHPCourse2020/08_functions.php
@@ -5,7 +5,33 @@
 // Create sum of two functions
 
 // Create function to sum all numbers using ...$nums
+/* $sum = function(...$numbers){
+    echo '<pre>';
+    var_dump($numbers);
+    echo '</pre>';
+}; */
+/* function sum(...$newArr){
+    echo '<pre>';
+    var_dump($newArr);
+    echo '</pre>';
+};
+sum(
+    'new',
+    'indexes',
+    'of',
+    'new array'
+); */
 
+function sum(...$newArr){
+    $sum = 0;
+    foreach($newArr as $num) $sum += $num; {
+        return $sum;
+    }
+};
+$sum = sum(
+    1, 2, 3, 4, 5
+);
+echo $sum . '<br />';
 // Arrow functions
 
 
--- PHPCourse2020/09_dates.php
+++ PHPCourse2020/09_dates.php
@@ -1,16 +1,28 @@
 <?php
 
-// Print current timestamp
-
+/* // Print current timestamp
+echo time() . ' (shows the time stamp) <br />';
 // Print current date
-
+echo date('Y-m-d H:i:s') . '<br />';
 // Print yesterday
+echo date('Y-m-d H:i:s', (time() - 60 * 60 *24)) . '<br />';
 
 // Different format: https://www.php.net/manual/en/function.date.php
-
+echo date(' F j Y, H:i:s') . '<br />';
 // String to timestamp
-
+echo strtotime('now') . '<br />';
+echo strtotime('+1 week') . '<br />'; */
 // Parse date: https://www.php.net/manual/en/function.date-parse.php
-
+/* $dateString = '2021-02-17 00:02:35';    // Declares date
+$parseDate = date_parse($dateString);
+echo '<pre>';
+var_dump($parseDate);
+echo '</pre>'; */
 // Parse date from format
 // https://www.php.net/manual/en/function.date-parse-from-format.php
+$dateString = 'February 17 2021 00:02:35'; // non-default format
+
+$parseDate = date_parse_from_format(' F j Y H:i:s', $dateString);
+echo '<pre>';
+var_dump($parseDate);
+echo '</pre>';
