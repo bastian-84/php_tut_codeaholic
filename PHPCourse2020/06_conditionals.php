50b65113a3478141df8c154a057b3bb2a3e11577
Sebastian Reichenbach <reichenbach.s@gmail.com>
2021-02-17 11:26:30 +0100

    seb, chapter 07 done

--- PHPCourse2020/06_conditionals.php
+++ PHPCourse2020/06_conditionals.php
@@ -1,6 +1,6 @@
 <?php
 
-$age = 20;
+$age = 28;
 $salary = 300000;
 
 // if condition
@@ -16,10 +16,56 @@ $salary = 300000;
 // if condition1 and condition2 - elseif condition1 and condition2 - else
 
 // Ternary if
-
+//echo ($age < 22) ? 'Young' : 'Not young';
+// echo ($age < 22) ? 'Young' : ($age < 30 ? 'Not young' : 'old');
 // Null coalescing operator
 
+// 7.1
+// exists?
+/* if($age == true){
+    echo var_dump($age) . ' ($age) exits ';
+} */
+/* $myAge = 0;
+if($myAge == true){
+    $myAge = 8;
+}else{
+    $myAge = 18;
+} */
+#$myAge = $age ? : 18;
+# if ($age == true){ $myAge = $age}
+
+/* $myAddress = isset($address) ? $address : 'Current location';
+$myAddress = $address ?? 'Current location';
+# if($address isset){ $address = $address}else{ echo ' Current location'}
+echo $myAddress; */
+
+
 // Null coalescing assignment operator. Since PHP 7.4
+/* $person = array(
+    'name' => 'John'
+); */
+/* if ( !isset($person['name'])){
+    $person['name'] = 'Anonymous';
+} */
 
+/* $person['name'] ??= 'Anonymous';
+
+echo $person['name'] . '<br />'; */
 // switch
+// vanilla php
+$userRole = 'admin';
 
+switch ($userRole){
+    case 'admin':
+        echo 'Your role is admin';
+        break;
+    case 'editor':
+        echo 'Your role is editor';
+        break;
+    default:
+        echo 'You are not assigened to a role.';
+        break;
+}
+// new syntax
+
+/* echo $myAge; */
--- PHPCourse2020/07_loops.php
+++ PHPCourse2020/07_loops.php
@@ -3,11 +3,48 @@
 // while
 
 // Loop with $counter
+/* $iCounter = 0;
+while($iCounter < 10){
+    echo 'Counter = ' . $iCounter . '<br />';
+    $iCounter++;
+    if( $iCounter >= 6){
+        break;
+    }
+} */
+/* $iCounter = 0;
+$bLoop = true;
+while($bLoop == true){
+    echo 'Counter = ' . $iCounter . '<br />';
+    $iCounter++;
+    if( $iCounter >= 6){
+        $bLoop = false;
+    }
+} */
 
 // do - while
 
 // for
 
 // foreach
+/* $fruits = array('Banana', 'Apple', 'Limette');
+foreach ($fruits as $fruit ) {
+    echo  $fruit . '<br />';
+}; */
 
 // Iterate Over associative array.
+$arrPerson = array(
+    'name' => 'Claire',
+    'surname' => 'Redfield',
+    'age' => '29',
+    'hobbies' => array(
+        'Shooting range',
+        'Riddles'
+    )
+);
+# var_dump($arrPerson);
+foreach($arrPerson as $key => $value){
+    if($key === 'hobbies'){
+        break;
+    }
+    echo $value . '<br />';
+}
